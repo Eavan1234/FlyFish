44d27afdb8623b37b3c8500d056f153fdaac32fe
Eavan1234 <liangyiwen1999@163.com>
2022-04-02 15:30:13 +0100

    Clean

--- miniprogram/envList.js
+++
@@ -1,6 +0,0 @@
-const envList = [{"envId":"cloud1-1g59gdccfb7ba079","alias":"cloud1"}]
-const isMac = true
-module.exports = {
-    envList,
-    isMac
-}
--- uploadCloudFunction.sh
+++
@@ -1,1 +0,0 @@
-"/Applications/wechatwebdevtools.app/Contents/MacOS/cli" cloud functions deploy --e cloud1-6gg3z3igf03960f1 --n quickstartFunctions --r --project "/Users/lichenming/WeChatProjects/miniprogram-2" --report_first --report
